d803828bdefb428bd4a4e6bbe702e193a4b16168
Luis Felipe Armijo <luis.armijo@axity.com>
2020-09-27 23:19:21 -0300

    modelo marca

---
+++ src/main/java/com/escalab/model/Marca.java
@@ -0,0 +1,50 @@
+package com.escalab.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "marca")
+public class Marca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idMarca;
+    @Column(name = "descripcion", length = 70)
+    private String descripcion;
+    @Column(name = "direccion", length = 70)
+    private String direccion;
+    @Column(name = "ciudad", length = 30)
+    private String ciudad;
+
+    public Long getIdMarca() {
+        return idMarca;
+    }
+
+    public void setIdMarca(Long idMarca) {
+        this.idMarca = idMarca;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+}
