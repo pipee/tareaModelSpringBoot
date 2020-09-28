61e4852a2d555347184be66ae5edab9707ac3d43
Luis Felipe Armijo <luis.armijo@axity.com>
2020-09-27 21:14:55 -0300

    primer commit

---
+++ src/test/java/com/escalab/TareaModelSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaModelSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
