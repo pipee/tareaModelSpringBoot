cc183d311a435ae7e9fb8ffddd011f9cec14961f
Luis Felipe Armijo <luis.armijo@axity.com>
2020-10-01 01:10:40 -0300

    relacion muchos a muchos compra producto

---
+++ src/main/java/com/escalab/model/CompraProducto.java
@@ -0,0 +1,33 @@
+package com.escalab.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compra_producto")
+@IdClass(CompraProductoPK.class)
+public class CompraProducto {
+
+    @Id
+    private Venta venta;
+    @Id
+    private Producto producto;
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
